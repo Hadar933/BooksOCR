abcf636f35ddb2743f80fad3f8f7f2a3c6f6b0a0
hadar <hadar933@gmail.com>
2024-02-28 21:17:21 +0200

    [ocr] added BGR2RGB conversion for tesseract image

--- ocr.py
+++ ocr.py
@@ -1,4 +1,6 @@
 from abc import ABC
+
+import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
@@ -23,5 +25,6 @@ class Tesseract(OCREngine):
 		super().__init__(language)
 
 	def __call__(self, image: np.ndarray) -> str:
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # pytesseract works with RGB images
 		result = pytesseract.image_to_string(image, lang=self.language)
 		return result
