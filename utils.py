32d2a7dfcd33156ef22b29176f647c461729fa64
hadar <hadar933@gmail.com>
2024-02-28 19:48:50 +0200

    [utils] utility functions

---
+++ utils.py
@@ -0,0 +1,18 @@
+import os
+
+import image_operators as io
+
+
+def save_ocr(
+		image_operator: io.ImageOperator,
+		ocr_text: str,
+		base_dir: str | None = None
+) -> None:
+	if not base_dir:
+		base_dir = os.path.join('resources', 'ocr')
+	ocr_path = os.path.join(
+		os.getcwd(),
+		os.path.join(base_dir, f'ocr_{image_operator.__str__()}.txt')
+	)
+	with open(ocr_path, 'w', encoding='utf-8') as file:
+		file.write(ocr_text)
